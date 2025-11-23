9dd8adb8440cc61e468b5049e1678ed3caa3065e
project55motors <nathan@project55motors.co.uk>
2025-11-23 13:03:25 +0000

    Delete admin-shortcut.js as now in Cloudflare as worker

--- admin-shortcut.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(e){
-    if(e.shiftKey && e.key.toLowerCase() === 'a'){
-        const adminContainer = document.getElementById('admin-container');
-        if(adminContainer){
-            adminContainer.style.display = 'block';
-            window.scrollTo(0,0);
-        }
-    }
-});
